5b727084466b4d0bcf425a3fbc1c8be587f45dfd
carlos-a-g-h <carloselputohabanero@proton.me>
2023-03-25 22:04:55 -0400

    Create aiorque.py

---
+++ aiorque/aiorque.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python3.9
+
+import asyncio
+import aiohttp
+import yarl
+
+def url_build(api_path,rque_home,rque_port=8080):
+	y=yarl.URL(rque_home)
+	u=y.scheme+"://"+y.host
+	if rque_port:
+		try:
+			int(rque_port)
+		except:
+			pass
+		else:
+			u=u+":"+str(rque_port)
+
+	return u+api_path
+
+async def rque_api(the_method="GET",api_path="/all",the_json=None,session=None,rque_home="http://127.0.0.1",rque_port=8080):
+
+	local=False
+	if not session:
+		local=True
+		session=aiohttp.ClientSession()
+		session.headers.update({"Content-Type":"application/json","Accept":"application/json",
+		})
+
+	the_url=url_build(api_path,rque_home,rque_port)
+	print(f"\n- rQUE call: {the_method} {the_url}\n  Data: {the_json}")
+	try:
+		async with session.request(method=the_method,url=the_url,json=the_json,verify_ssl=False) as response:
+			rsp_json=await response.json()
+			if not str(response.status).startswith("2"):
+				raise Exception(f"Non-valid status code of {response.status}")
+	except Exception as e:
+		print("  error:",e)
+		result={}
+	else:
+		result=rsp_json
+
+	if local:
+		await session.close()
+
+	print("  result:",result)
+	return result
+
+async def rque_get_all(rque_home="http://127.0.0.1",rque_port=8080,session=None):
+	res=await rque_api(session=session,rque_home=rque_home,rque_port=rque_port)
+	return res
+
+async def rque_get_group(group_name,rque_home="http://127.0.0.1",rque_port=8080,session=None):
+	res=await rque_api(session=session,api_path=f"/sel/{group_name}",rque_home=rque_home,rque_port=rque_port)
+	return res
+
+async def rque_get_item(group_name,index,rque_home="http://127.0.0.1",rque_port=8080,session=None):
+	res=await rque_api(session=session,api_path=f"/sel/{group_name}/{index}",rque_home=rque_home,rque_port=rque_port)
+	return res
+
+async def rque_get_range(group_name,index,qtty,rque_home="http://127.0.0.1",rque_port=8080,session=None):
+	res=await rque_api(session=session,api_path=f"/sel/{group_name}/{index}/{qtty}",rque_home=rque_home,rque_port=rque_port)
+	return res
+
+async def rque_addsin(group_name,item,rque_home="http://127.0.0.1",rque_port=8080,session=None):
+	res=await rque_api(session=session,the_method="POST",the_json={"name":group_name,"item":item},api_path="/add/sin",rque_home=rque_home,rque_port=rque_port)
+	return res
+
+async def rque_addmul(group_name,item_list,rque_home="http://127.0.0.1",rque_port=8080,session=None):
+	res=await rque_api(session=session,the_method="POST",the_json={"name":group_name,"list":item_list},api_path="/add/mul",rque_home=rque_home,rque_port=rque_port)
+	return res
+
+async def rque_del_all(rque_home="http://127.0.0.1",rque_port=8080,session=None):
+	res=await rque_api(session=session,the_method="DELETE",rque_port=rque_port)
+	return res
+
+async def rque_del_group(group_name,rque_home="http://127.0.0.1",rque_port=8080,session=None):
+	res=await rque_api(session=session,the_method="DELETE",api_path=f"/sel/{group_name}",rque_home=rque_home,rque_port=rque_port)
+	return res
+
+async def rque_del_item(group_name,index,rque_home="http://127.0.0.1",rque_port=8080,session=None):
+	res=await rque_api(session=session,the_method="DELETE",api_path=f"/sel/{group_name}/{index}",rque_home=rque_home,rque_port=rque_port)
+	return res
+
+async def rque_del_range(group_name,index,qtty,rque_home="http://127.0.0.1",rque_port=8080,session=None):
+	res=await rque_api(session=session,the_method="DELETE",api_path=f"/sel/{group_name}/{index}/{qtty}",rque_home=rque_home,rque_port=rque_port)
+	return res
+
+class rque_Client:
+
+	def __init__(self,session=None,addr="http://127.0.0.1",port=8080):
+		self.addr=addr
+		self.port=port
+
+		if session:
+			self.session=session
+
+		if not session:
+			self.session=aiohttp.ClientSession()
+			self.session.headers.update({
+				"Content-Type":"application/json",
+				"Accept":"application/json",
+			})
+
+	async def close(self):
+		if self.session:
+			await self.session.close()
+			print("[!] Client closed")
+		if not self.session:
+			print("[!] Nothing to close")
+
+	async def __aenter__(self):
+		return self
+
+	async def __aexit__(self,*excinfo):
+		await self.close()
+
+	async def get_item(self,gname,index):
+		res=await rque_get_item(gname,index,rque_home=self.addr,rque_port=self.port,session=self.session)
+		return res
+
+	async def get_range(self,gname,index,qtty):
+		res=await rque_get_range(gname,index,qtty,rque_home=self.addr,rque_port=self.port,session=self.session)
+		return res
+
+	async def add_sin(self,gname,item):
+		res=await rque_addsin(gname,item,rque_home=self.addr,rque_port=self.port,session=self.session)
+		return res
+
+	async def add_mul(self,gname,item_list):
+		res=await rque_addmul(gname,item_list,rque_home=self.addr,rque_port=self.port,session=self.session)
+		return res
+
+	async def del_item(self,gname,index):
+		res=await rque_del_item(gname,index,rque_home=self.addr,rque_port=self.port,session=self.session)
+		return res
+
+	async def del_range(self,gname,index,qtty):
+		res=await rque_del_range(gname,index,qtty,rque_home=self.addr,rque_port=self.port,session=self.session)
+		return res
