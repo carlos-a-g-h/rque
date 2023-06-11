1d326b6602f4be502618169ef4324e496c1beed0
carlos-a-g-h <carloselputohabanero@proton.me>
2023-06-11 18:04:33 -0400

    Update main.rs

--- _/src/main.rs
+++ _/src/main.rs
@@ -1,3 +1,5 @@
+mod data_storage;
+
 use std::collections::HashMap;
 use std::env;
 use std::sync::Mutex;
@@ -5,6 +7,9 @@ use actix_web::{get, post, delete, web, App, HttpServer, HttpResponse, HttpReque
 use actix_web::http::{header, StatusCode};
 use serde::Deserialize;
 use serde_json::json;
+
+use crate::data_storage::Group;
+use crate::data_storage::Storage;
 
 static RQUE_DEFAULT_PORT:u16=8080;
 
@@ -103,103 +108,6 @@ static RQUE_HELP:&str="
 	</body>
 </html>
 ";
-
-// Group struct
-
-struct Group { data: Vec<Vec<String>> }
-
-impl Group
-{
-	fn new() -> Group { Group { data:Vec::new() } }
-
-	fn get_size(&self) -> usize { self.data.len() }
-
-	fn is_empty(&self) -> bool { let size=self.get_size();if size==0 { true } else { false } }
-
-	fn index_exists(&self,index:usize) -> bool { let size=self.get_size();if index>size || size==0 || size==index { false } else { true } }
-
-	fn get(&self,index: usize) -> Vec<String> { if self.index_exists(index) { self.data[index].clone() } else { Vec::new() } }
-
-	fn has_head(&self,head: &String) -> bool
-	{
-		if self.is_empty()
-		{
-			return false;
-		};
-		let mut rep=false;
-		for elem in &self.data
-		{
-			let elem_head=elem.first().unwrap();
-			if elem_head==head
-			{
-				rep=true;break;
-			};
-		};
-		rep
-	}
-
-	fn add(&mut self,value: Vec<String>) -> bool
-	{
-		if value.len()==0
-		{
-			return false;
-		};
-		let val_head=value.first().unwrap();
-		if self.has_head(val_head)
-		{
-			return false;
-		};
-		self.data.push(value);
-		true
-	}
-
-	fn kick(&mut self,index: usize) -> Vec<String> { if self.index_exists(index) { self.data.remove(index) } else { Vec::new() } }
-
-	fn get_range(&mut self,index: usize, qtty: usize, steal: bool) -> Vec<Vec<String>>
-	{
-		if !self.index_exists(index)
-		{
-			return Vec::new()
-		};
-		let size=self.get_size();
-		let qtty_real:usize={ if qtty==0 { size } else { qtty } };
-		let mut result:Vec<Vec<String>>=Vec::new();
-		let mut pos=index;
-		let mut added:usize=0;
-		loop
-		{
-			let elem:Vec<String>={
-				if steal { self.kick(pos) } else { self.get(pos) }
-			};
-			if elem.len()==0
-			{
-				break;
-			};
-			result.push(elem.to_vec());
-			if !steal
-			{
-				pos=pos+1;
-			};
-			added=added+1;
-			if pos==size || added==qtty_real
-			{
-				break;
-			};
-		};
-		result
-	}
-}
-
-// Main Data struct
-
-struct Storage { quecol: HashMap<String,Group> }
-
-impl Storage
-{
-	fn get_size(&self) -> usize { self.quecol.len() }
-
-	fn is_empty(&self) -> bool { return self.quecol.is_empty() }
-}
 
 // Application Data in a Mutex
 
@@ -774,7 +682,8 @@ async fn main() -> std::io::Result<()>
 	);
 
 	let pdata=web::Data::new(TheAppState{
-		holder: Mutex::new( Storage{ quecol: HashMap::new() } )
+		//holder: Mutex::new( Storage{ quecol: HashMap::new() } )
+		holder: Mutex::new( Storage::new() } )
 	});
 
 	HttpServer::new(move ||
